def1e35d395922195471b414a4a9c248aec86b7c
Analista Abel <abelbrasil88@gmail.com>
2025-06-21 16:37:51 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Jornada de dados")
+
+# Somar dois números 
+
+a1 = 2
+a2 = 3
+soma = sum([a1,a2])
+print(soma)
+
+# Variaveis
+idade = 37
+nome = "abel"
+print(nome)
+
+# Lista
+lista = [1,2,3,4,5]
+
